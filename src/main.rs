5f38adef86ecb2a13bcb09f022e74374806cfe86
hayasshi <daisuke0884@gmail.com>
2021-04-10 23:30:53 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{
+    get,
+    web::{self},
+    App, HttpServer, Responder,
+};
 
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
